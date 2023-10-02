379e97794365f1ed7db52c51e0976f1ddecc9923
link-op <40344950+14link@users.noreply.github.com>
2023-10-02 19:53:19 +0800

    lienol

---
+++ lienol-p1.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# Add a feed source
+		sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+		sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+
+		sed -i '$a src-git diypkg https://github.com/14link/diy-package' feeds.conf.default
+
+	#	 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+	#	 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+	#	 sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+	#	 mv -vf ../mt7621_phicomm_k2p.dts ./target/linux/ramips/dts/mt7621_phicomm_k2p.dts
+	#	 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+	#	 svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/aliyundrive-webdav
+	#	 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+	#	 rm -rf ./package/feeds/kenzo/luci-theme-edge 
+	#	 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
--- lienol-p2.sh
+++ lienol-p2.sh
@@ -23,8 +23,11 @@ sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/default-settings/files/
 
 #sed -i '/DTS_DIR:=$(LINUX_DIR)/a\BUILD_DATE_PREFIX := $(shell date +'%F')' include/image.mk
 #sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(BUILD_DATE_PREFIX)-/g' include/image.mk
-sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt [$(TZ=UTC-8 date "+%Y.%m.%d")] Compiled by kS '/g" package/default-settings/files/zzz-default-settings
+sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt [$(TZ=UTC-8 date "+%Y.%m.%d")] Compiled by kSir '/g" package/default-settings/files/zzz-default-settings
 sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-kS'/g" ./package/base-files/files/bin/config_generate
+		
+git clone https://github.com/sirpdboy/autosamba package/autosamba
+git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
 
 # curl -fsSL  https://raw.githubusercontent.com/Lienol/openwrt-packages/19.07/net/https-dns-proxy/files/https-dns-proxy.config > ./feeds/packages/net/https-dns-proxy/files/https-dns-proxy.config
 # curl -fsSL  https://raw.githubusercontent.com/Lienol/openwrt-packages/19.07/net/https-dns-proxy/files/https-dns-proxy.init > ./feeds/packages/net/https-dns-proxy/files/https-dns-proxy.init

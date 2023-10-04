f0e7e9d1c19d42cb4a783619232d30208ad138cd
link-op <40344950+14link@users.noreply.github.com>
2023-10-04 21:22:55 +0800

    更新 diy-part1-6.sh

--- diy-part1-6.sh
+++ diy-part1-6.sh
@@ -16,7 +16,6 @@
 # Add a feed source
 		sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 		sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-
 		sed -i '$a src-git diypkg https://github.com/14link/diy-package' feeds.conf.default
 	#	git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
 		

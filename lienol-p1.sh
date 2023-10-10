090eaaef10b2525406925ab24057874424db8360
link-op <40344950+14link@users.noreply.github.com>
2023-10-10 20:53:17 +0800

    更新 lienol-p1.sh

--- lienol-p1.sh
+++ lienol-p1.sh
@@ -19,9 +19,9 @@
 		sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 		sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 		sed -i '$a src-git diypkg https://github.com/14link/diy-package;22.03' feeds.conf.default
-		echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
-		echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-		echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
+	#	echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+	#	echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+	#	echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 		
 		
 		

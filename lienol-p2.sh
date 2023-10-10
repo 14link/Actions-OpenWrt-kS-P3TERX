1447f262cafa020360d51423bb4ab582a90f466e
link-op <40344950+14link@users.noreply.github.com>
2023-10-10 22:31:27 +0800

    更新 lienol-p2.sh

--- lienol-p2.sh
+++ lienol-p2.sh
@@ -43,7 +43,7 @@ rm -rf ./feeds/kenzo/luci-theme-tomato
 #mosdns
 rm -rf ./feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
-rm -rf feeds/packages/net/v2ray-geodata
+rm -rf ./feeds/packages/net/v2ray-geodata
 git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 #

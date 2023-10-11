da05a77fa4c3ae2e2bf8c883d75e3b633600f169
link-op <40344950+14link@users.noreply.github.com>
2023-10-11 13:52:06 +0800

    更新 lienol-p1.sh

--- lienol-p1.sh
+++ lienol-p1.sh
@@ -13,7 +13,6 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
-	#	sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' feeds.conf.default
 		sed -i 's/192.168.1.1/192.168.0.252/g' package/base-files/files/bin/config_generate
 		sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/default-settings/files/zzz-default-settings
 		sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
@@ -22,7 +21,7 @@
 	#	echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 	#	echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
 	#	echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
-		
+	#	sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages.git' feeds.conf.default
 		
 		
 	#	 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default

ebc719fb03c733e5e93ea8d794fb5ac81f043958
link-op <40344950+14link@users.noreply.github.com>
2023-11-28 15:52:54 +0800

    samba4

--- diy-part2.sh
+++ diy-part2.sh
@@ -26,11 +26,11 @@
 		sed -i "s/DISTRIB_DESCRIPTION='OpenWrt '/DISTRIB_DESCRIPTION='OpenWrt [$(TZ=UTC-8 date "+%Y.%m.%d")] Compiled by kSir '/g" package/lean/default-settings/files/zzz-default-settings
 		sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-kS'/g" ./package/base-files/files/bin/config_generate
 		#调整&&去重,删除不使用的源文件
-		rm -rf ./package/lean/autosamba
+	#	rm -rf ./package/lean/autosamba
 		rm -rf ./feeds/luci/applications/luci-app-netdata
 	#	rm -rf package/lean/autosamba
 
-		git clone https://github.com/sirpdboy/autosamba package/autosamba
+	#	git clone https://github.com/sirpdboy/autosamba package/autosamba
 		git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
 		git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge.git package/luci-theme-edge
 	#	git clone -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
